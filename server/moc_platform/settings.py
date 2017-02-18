8b7ca32b02f2bb49b568a7de2e46b04cd166bb7e
David Burke <david@burkesoftware.com>
2017-02-18 18:37:19 -0500

    heroku

--- server/moc_platform/settings.py
+++ server/moc_platform/settings.py
@@ -142,6 +142,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 MEDIA_URL = '/media/'
 
 WAGTAIL_SITE_NAME = "moc donation platform"
